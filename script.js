dcfd4c250778aa22742c11e70b4fe0c738733f2b
kiptubei <kiptubei@gmail.com>
2020-10-01 12:17:17 +0300

    Added myFunction for displaying form data

--- script.js
+++ script.js
@@ -12,17 +12,37 @@ function addBookToLibrary() {
   myLibrary.push(myBook);
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+// addBookToLibrary();
+// console.log(myLibrary);
 
-let table = document.getElementById('di-library');
-myLibrary.forEach(library => {
-    let tr = document.createElement('tr');
-    const values = Object.values(library);
-    values.forEach(value => {
-       let td = document.createElement('td');
-       td.innerHTML= value;
-       tr.appendChild(td);
-    });
-    table.appendChild(tr);
-});
+// let table = document.getElementById("di-library");
+// myLibrary.forEach((library) => {
+//   let tr = document.createElement("tr");
+//   const values = Object.values(library);
+//   values.forEach((value) => {
+//     let td = document.createElement("td");
+//     td.innerHTML = value;
+//     tr.appendChild(td);
+//   });
+//   table.appendChild(tr);
+// });
+
+// let author = document.getElementById("author").value;
+// let title = document.getElementByName("title").value;
+// let page = document.getElementByName("pages").value;
+//
+// console.log(author);
+// console.log(title);
+// console.log(pages);
+
+function myFunction() {
+  var x = document.getElementById("myForm");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length; i++) {
+    text += x.elements[i].value + "<br>";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+
+console.log("script.js");
