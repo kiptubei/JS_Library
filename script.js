8696110b14a046a62013481732ae5478e6c5c47c
kiptubei <kiptubei@gmail.com>
2020-10-01 17:42:58 +0300

    Fixed populate books list

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 let myLibrary = [];
 
-  let table = document.getElementById('di-library');
-
-  myLibrary.forEach(library => {
-      let tr = document.createElement('tr');
-      console.log(myLibrary.length);
-      let values = Object.values(library);
-      values.forEach(value => {
-        let td = document.createElement('td');
-        td.innerHTML= value;
-        tr.appendChild(td);
-      });
-      table.appendChild(tr);
-  });
-
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
@@ -23,17 +9,27 @@ function Book(author, title, pages, read) {
 
 function addBookToLibrary(myBook) {
   myLibrary.push(myBook);
-  console.log(myLibrary);
+  let table = document.getElementById("di-library");
+  myLibrary.forEach((library) => {
+    let tr = document.createElement("tr");
+    console.log(myLibrary.length);
+    let values = Object.values(library);
+    values.forEach((value) => {
+      let td = document.createElement("td");
+      td.innerHTML = value;
+      tr.appendChild(td);
+    });
+    table.appendChild(tr);
+  });
 }
 
-let form = document.querySelector('.form');
-form.addEventListener('submit', (e) => {
+let form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const author = document.querySelector('#author').value;
-  const title = document.querySelector('#title').value;
-  const pages = document.querySelector('#pages').value;
-  const read = document.querySelector('#read').checked;
-  let book = new Book(author, title, pages, read);
-  console.log(book);
+  const author = document.querySelector("#author").value;
+  const title = document.querySelector("#title").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+  const book = new Book(author, title, pages, read);
   addBookToLibrary(book);
 });
