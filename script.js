1e5ee6e8eaf090ed7a1c5c8070f06898c0c9589e
tGodson <tendongze95@gmail.com>
2020-10-01 14:07:11 +0100

    edit input form

--- script.js
+++ script.js
@@ -7,18 +7,31 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  const myBook = new Book("Chinua Achebe", "Things Fall Apart", 250, false);
+function addBookToLibrary(myBook) {
+  let book = new Book("author", "title", 300, true);
   myLibrary.push(myBook);
+  console.log(myLibrary.length);
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
+  const book = new Book(author, title, pages, read);
+  console.log(book);
+  addBookToLibrary(book);
+});
 
 let table = document.getElementById('di-library');
+
 myLibrary.forEach(library => {
+  console.log("present");
     let tr = document.createElement('tr');
-    const values = Object.values(library);
+    console.log(myLibrary.length);
+    let values = Object.values(library);
     values.forEach(value => {
        let td = document.createElement('td');
        td.innerHTML= value;
