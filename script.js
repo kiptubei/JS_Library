5e9adf938399165c9e44efda24fc3f84585ef9b9
tGodson <tendongze95@gmail.com>
2020-10-01 14:56:28 +0100

    modify foreach loop

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let myLibrary = [];
 
+  let table = document.getElementById('di-library');
+
+  myLibrary.forEach(library => {
+      let tr = document.createElement('tr');
+      console.log(myLibrary.length);
+      let values = Object.values(library);
+      values.forEach(value => {
+        let td = document.createElement('td');
+        td.innerHTML= value;
+        tr.appendChild(td);
+      });
+      table.appendChild(tr);
+  });
+
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
@@ -8,34 +22,18 @@ function Book(author, title, pages, read) {
 }
 
 function addBookToLibrary(myBook) {
-  let book = new Book("author", "title", 300, true);
   myLibrary.push(myBook);
-  console.log(myLibrary.length);
+  console.log(myLibrary);
 }
 
-const form = document.querySelector('.form');
+let form = document.querySelector('.form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const author = document.querySelector('#author').value;
   const title = document.querySelector('#title').value;
   const pages = document.querySelector('#pages').value;
   const read = document.querySelector('#read').checked;
-  const book = new Book(author, title, pages, read);
+  let book = new Book(author, title, pages, read);
   console.log(book);
   addBookToLibrary(book);
-});
-
-let table = document.getElementById('di-library');
-
-myLibrary.forEach(library => {
-  console.log("present");
-    let tr = document.createElement('tr');
-    console.log(myLibrary.length);
-    let values = Object.values(library);
-    values.forEach(value => {
-       let td = document.createElement('td');
-       td.innerHTML= value;
-       tr.appendChild(td);
-    });
-    table.appendChild(tr);
 });
